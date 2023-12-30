48da4e72fe5d1427185ea45398335d00e24f7ba2
ahmedelhdad123 <ahmedelhdad956@gmail.com>
2023-12-31 00:11:06 +0200

    Create Database Closes #1

---
+++ src/main/java/com/spring/restaurant/entity/Category.java
@@ -0,0 +1,40 @@
+package com.spring.restaurant.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.util.Date;
+import java.util.Set;
+
+
+@Entity
+@Table(name = "category")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String categoryName;
+
+    @Column(name = "date_create")
+    @CreationTimestamp
+    private Date dateCreate;
+
+    @Column(name = "date_update")
+    @UpdateTimestamp
+    private Date dateUpdate;
+
+    @OneToMany(mappedBy = "category")
+    private Set<Order> orders;
+}
---
+++ src/main/java/com/spring/restaurant/entity/Order.java
@@ -0,0 +1,50 @@
+package com.spring.restaurant.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "orders")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String orderName;
+
+
+    @Column(name = "price")
+    private int price;
+
+
+    @Column(name = "image")
+    private String img;
+
+    @Column(name = "description")
+    @Lob  // to set big size to write description because default size 256 character
+    private String description;
+
+    @Column(name = "date_create")
+    @CreationTimestamp
+    private Date dateCreate;
+
+    @Column(name = "date_update")
+    @UpdateTimestamp
+    private Date dateUpdate;
+
+    @ManyToOne
+    @JoinColumn(name = "id_category")
+    private Category category;
+}
